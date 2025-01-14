89979fbe7b6732dd9f3ed8677504a021e4bbbfba
Bishnu Suyel <suyel2007@gmail.com>
2025-01-14 10:16:42 +0200

    add new route for updating the task

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
     Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
     Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
+    Route::patch('/tasks/{task}', [TaskController::class, 'update'])->name('tasks.update');
 });
 
 
