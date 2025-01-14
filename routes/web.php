647180301b3a72c0589a1d4f76481c723e15d9d5
Bishnu Suyel <suyel2007@gmail.com>
2025-01-14 12:39:16 +0200

    Add new routes to toggle update & edit task

--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth')->group(function () {
     Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
     Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
+    Route::get('/tasks/{task}/edit', [TaskController::class, 'edit'])->name('tasks.edit');
     Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
     Route::patch('/tasks/{task}', [TaskController::class, 'update'])->name('tasks.update');
+    Route::patch('/tasks/{task}/toggle', [TaskController::class, 'toggleCompletion'])->name('tasks.toggle');
+
 });
 
 
